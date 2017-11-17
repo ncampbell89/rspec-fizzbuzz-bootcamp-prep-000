4c931029ce93cd17b19eee3b455f4bb81e11fc1a
Nicole Campbell <nscampbell303@gmail.com>
2017-11-17 22:18:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(fizz_3, fizz_5, fizz_15)
-  if fizz_3 % 3 == 0
+def fizzbuzz(init)
+  if init % 3 == 0
     "Fizz"
-  elsif fizz_5 % 5 == 0
+  elsif init % 5 == 0
     "Buzz"
-  elsif fizz_15 % 3 == 0 && 5 == 0
+  elsif init % 3 == 0 && 5 == 0
     "FizzBuzz"
   else
     nil
