f2a6f267b118c998745a7139a19092fdaa610c07
Nicole Campbell <nscampbell303@gmail.com>
2017-11-17 21:49:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,5 @@
 require_relative './spec/fizzbuzz_spec.rb'
 
-if fizzbuzz == fizz_3
-  puts "Fizz"
-elsif fizzbuzz == fizz_5
-  puts "Buzz"
-elsif fizzbuzz == fizz_15
-  puts "FizzBuzz"
-else
-  puts "nil"
+def fizzbuzz
+
+end
